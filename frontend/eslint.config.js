a21e3101dc86403eab880fb8b7edd2a047cf5f4e
Seva <20211206nch@mail.ru>
2025-09-20 21:04:44 +0300

    modified  eslint.config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,87 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  {
+    ignores: ['dist', 'node_modules'],
+  },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettier,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-])
+  eslintConfigPrettier, // Отключает правила ESLint, которые конфликтуют с Prettier
+];
+
+// import js from '@eslint/js';
+// import reactHooks from 'eslint-plugin-react-hooks';
+// import reactRefresh from 'eslint-plugin-react-refresh';
+// import globals from 'globals';
+// import tseslint from 'typescript-eslint';
+// import prettier from 'eslint-plugin-prettier';
+// import eslintConfigPrettier from 'eslint-config-prettier';
+
+// export default tseslint.config(
+//   {
+//     ignores: ['dist', 'node_modules'],
+//   },
+//   {
+//     files: ['**/*.{js,jsx}'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//       parserOptions: {
+//         ecmaFeatures: { jsx: true },
+//       },
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//       prettier: prettier,
+//     },
+//     rules: {
+//       ...js.configs.recommended.rules,
+//       ...reactHooks.configs.recommended.rules,
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//       'prettier/prettier': 'error',
+//     },
+//     settings: {
+//       react: {
+//         version: 'detect',
+//       },
+//     },
+//   },
+//   eslintConfigPrettier // Отключает правила ESLint, которые конфликтуют с Prettier
+// );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@pages': path.resolve('./src/pages'),
+      '@modules': path.resolve('./src/modules'),
+      '@components': path.resolve('./src/entities'),
+      '@ui': path.resolve('./src/ui'),
+      '@hooks': path.resolve('./src/hooks'),
+    },
+  },
+});
