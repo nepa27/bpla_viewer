23b420146b5bdfbe24732a810bf5b024c7acc031
nepale <nepale@mail.ru>
2025-09-22 13:20:38 +0300

    Create models

---
+++ backend/app/models/flight.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Date, Time, DateTime, Interval, ForeignKey, func
+from sqlalchemy.orm import relationship
+from geoalchemy2 import Geometry
+from app.database import Base
+
+
+class Flight(Base):
+    __tablename__ = "flights"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=True)
+    flight_id = Column(String, unique=True, index=True, nullable=True)
+    drone_type = Column(String, nullable=True)
+    takeoff_coordinates = Column(Geometry('POINT', srid=4326), nullable=True)
+    landing_coordinates = Column(Geometry('POINT', srid=4326), nullable=True)
+    flight_date = Column(Date, nullable=True)
+    takeoff_time = Column(Time, nullable=True)
+    landing_time = Column(Time, nullable=True)
+    flight_duration = Column(Interval, nullable=True)
+
+    region_id = Column(Integer, ForeignKey("regions.id"), nullable=False, index=True)
+
+    region_rel = relationship("Region", back_populates="flights")
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
---
+++ backend/app/models/region.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Region(Base):
+    __tablename__ = "regions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=True)
+
+    flights = relationship("Flight", back_populates="region_rel")
