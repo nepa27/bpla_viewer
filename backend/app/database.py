c3c064d9fbc34a6035372411d4ea5976d33f2c35
nepa27 <nepale@mail.ru>
2025-09-23 14:52:42 +0300

    Add database config

---
+++ backend/app/database.py
@@ -0,0 +1,48 @@
+from os import getenv
+from typing import Any, AsyncGenerator
+
+from dotenv import load_dotenv
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from sqlalchemy.orm import declarative_base
+
+load_dotenv()
+
+DB_HOST = getenv('DB_HOST', 'localhost')
+DB_PORT = getenv('DB_PORT', '5432')
+DB_USER = getenv('DB_USER', 'postgres')
+DB_PASS = getenv('DB_PASS', 'password')
+DB_NAME = getenv('DB_NAME', 'flight_db')
+SCHEMA_NAME = getenv('SCHEMA_NAME', 'your_schema_name')
+
+DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+Base = declarative_base()
+
+async_engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False
+)
+
+async def get_db() -> AsyncGenerator[AsyncSession | Any, Any]:
+    async with AsyncSessionLocal() as session:
+        await session.execute(text(f"SET search_path TO {SCHEMA_NAME}"))
+        try:
+            yield session
+        finally:
+            await session.close()
+
+async def create_tables():
+    """Создает все таблицы в базе данных"""
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
