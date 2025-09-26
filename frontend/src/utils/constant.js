f72fbe7e4a562ad16c0c1b21c39a523d0d9b8e01
Seva <20211206nch@mail.ru>
2025-09-26 07:16:37 +0300

    feat: added file constant

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,7 @@
+export const SORT_OPTIONS_KEY = { top10: 'top10', desc: 'desc', asc: 'asc', all: 'all' };
+
+export const SORT_OPTIONS_CHART = [
+  { value: SORT_OPTIONS_KEY.desc, label: 'Топ 10 регионов' },
+  { value: SORT_OPTIONS_KEY.asc, label: 'По возрастанию (Топ 10)' },
+  { value: SORT_OPTIONS_KEY.all, label: 'Показать весь список регионов' },
+];
