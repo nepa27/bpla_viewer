21477bfc177d9d50b061849be5ad3a129441378c
Seva <20211206nch@mail.ru>
2025-09-26 18:20:14 +0300

    feat: react-router used (routes.js, GoBackProvider, NotFoundPage, App, main)

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,46 @@
 /* eslint-disable no-unused-vars */
+import { lazy } from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
 import RussianMapPage from '../pages/RussianMapPage';
+import GoBackProvider from '../utils/GoBackProvider';
+import ROUTES from '../utils/routes';
 import './App.css';
 
+const OneRegionMapPage = lazy(() => import('../pages/OneRegionMapPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
+
 const App = () => {
-  return <RussianMapPage />;
+  return (
+    <Routes>
+      {/* <Route path={ROUTES.SIGN_UP} element={<SignUpForm />} />
+      <Route path={ROUTES.SIGN_IN} element={<SignInForm />} /> */}
+
+      <Route path={ROUTES.HOME} element={<RussianMapPage />} />
+      <Route path={ROUTES.REGIONS} element={<RussianMapPage />} />
+      <Route
+        path={ROUTES.REGION_ONE}
+        element={
+          <GoBackProvider>
+            <OneRegionMapPage />
+          </GoBackProvider>
+        }
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 };
 
 export default App;
+
+// /* eslint-disable no-unused-vars */
+// import OneRegionMapPage from '../pages/OneRegionMapPage';
+// import RussianMapPage from '../pages/RussianMapPage';
+// import './App.css';
+
+// const App = () => {
+//   return <RussianMapPage />;
+// };
+
+// export default App;
---
+++ frontend/src/utils/routes.js
@@ -0,0 +1,9 @@
+const ROUTES = {
+  HOME: '/',
+  SIGN_UP: '/sign-up',
+  SIGN_IN: '/sign-in',
+  REGIONS: '/regions',
+  REGION_ONE: '/regions/:id',
+};
+
+export default ROUTES;
