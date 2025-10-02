0250b8a7b74594bebacfc14de3806d51488259f5
Seva <20211206nch@mail.ru>
2025-10-02 22:43:25 +0300

    fix: added in main queryClient RQ

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
@@ -7,10 +9,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
