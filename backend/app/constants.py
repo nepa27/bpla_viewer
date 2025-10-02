84169d17e77175d9d34af43dc8a3f965218c4bea
nepa27 <nepale@mail.ru>
2025-10-02 15:45:20 +0300

    Add constants for export to excel

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,2 +1,9 @@
 RUSSIA_FULL_POLYGON = "russia_regions.geo_full_rounded_coordinates.geo.json"
 RUSSIA_ID_POLYGON = "russia_regions_id.geo.json.gz"
+
+# Constants for export to xlsx
+
+WORD_NAME_OF_REGION = "O"
+WORD_COUNT_OF_FLIGHT = "P"
+PATH_TO_TEMPLATE_EXCEL = "export_template.xlsx"
+NUMBER_OF_ROW = 2
