f4312af2efc8d0ab220c88e05faa5b07c5b5a2cb
Seva <20211206nch@mail.ru>
2025-10-04 14:32:47 +0300

    feat: styled MapSkeleton with custom loader

--- frontend/src/utils/skeletons.jsx
+++ frontend/src/utils/skeletons.jsx
@@ -1,11 +1,30 @@
 // eslint-disable-next-line no-unused-vars
 import { Skeleton, Space, Spin } from 'antd';
 
+import styles from './MapSkeleton.module.css';
+
+// export const MapSkeleton = () => (
+//   <div className="map-container">
+//     <Skeleton active paragraph={{ rows: 0 }} style={{ height: '600px' }}>
+//       <Spin>Загрузка...</Spin>
+//     </Skeleton>
+//   </div>
+// );
 export const MapSkeleton = () => (
-  <div className="map-container">
-    <Skeleton active paragraph={{ rows: 0 }} style={{ height: '600px' }}>
-      <Spin>Загрузка...</Spin>
-    </Skeleton>
+  <div
+    className="map-container"
+    style={{ height: '600px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+  >
+    {/* <div 
+    className="map-container" 
+    style={{ 
+      height: '600px', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      backgroundColor: '#1a1a1a' // тёмный фон
+    }} */}
+    <span className={styles['loader']}></span>
   </div>
 );
 
