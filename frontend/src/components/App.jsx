f92e61114d22d2ac4f02a87c45b770990c217060
Seva <20211206nch@mail.ru>
2025-10-08 22:22:38 +0300

    fix: used hoc RequireAuth in App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,6 +4,8 @@ import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from '../components/Layout';
+import { AuthProvider } from '../hoc/AuthProvider';
+import RequireAuth from '../hoc/RequireAuth';
 import RussianMapPage from '../pages/RussianMapPage';
 import ROUTES from '../utils/routes';
 import './App.css';
@@ -15,32 +17,8 @@ const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
 const App = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route
-          path={ROUTES.HOME}
-          element={
-            <Suspense fallback={<div>Загрузка...</div>}>
-              <RussianMapPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path={ROUTES.REGIONS}
-          element={
-            <Suspense fallback={<div>Загрузка...</div>}>
-              <RussianMapPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path={ROUTES.REGION_ONE}
-          element={
-            <Suspense fallback={<div>Загрузка...</div>}>
-              <OneRegionMapPage />
-            </Suspense>
-          }
-        />
+    <AuthProvider>
+      <Routes>
         <Route
           path={ROUTES.SIGN_IN}
           element={
@@ -57,16 +35,47 @@ const App = () => {
             </Suspense>
           }
         />
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<div>Загрузка...</div>}>
-              <NotFoundPage />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
+        <Route element={<Layout />}>
+          <Route
+            path={ROUTES.HOME}
+            element={
+              <Suspense fallback={<div>Загрузка...</div>}>
+                <RussianMapPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.REGIONS}
+            element={
+              <Suspense fallback={<div>Загрузка...</div>}>
+                <RequireAuth>
+                  <RussianMapPage />
+                </RequireAuth>
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.REGION_ONE}
+            element={
+              <Suspense fallback={<div>Загрузка...</div>}>
+                <RequireAuth>
+                  <OneRegionMapPage />
+                </RequireAuth>
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<div>Загрузка...</div>}>
+                <NotFoundPage />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 };
 
