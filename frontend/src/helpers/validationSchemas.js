fa6d7660b71e3e299db00024e92e4315fb2b6c55
Seva <20211206nch@mail.ru>
2025-10-25 11:46:18 +0300

    fix: cleaned code validationSchemas, AuthProvider, RequireAuth

--- frontend/src/helpers/validationSchemas.js
+++ frontend/src/helpers/validationSchemas.js
@@ -1,16 +1,8 @@
 import * as yup from 'yup';
 
 const RegExpEmail = /(^[a-z][a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)/;
-const RegExpUser = /^[a-zA-ZА-Яа-я0-9]+$/;
 
 export const SignUpSchema = yup.object().shape({
-  username: yup
-    .string()
-    .trim()
-    .required('Это поле обязательно!')
-    .matches(RegExpUser, 'Можно использовать только русские и латинские буквы и цифры')
-    .min(3, 'Минимально 3 символа')
-    .max(25, 'Максимальная длина 25 символов'),
   email: yup
     .string()
     .required('Это поле обязательно!')
@@ -18,16 +10,12 @@ export const SignUpSchema = yup.object().shape({
   password: yup
     .string()
     .required('Это поле обязательно!')
-    .min(6, 'Минимально 6 символов')
+    .min(5, 'Минимально 5 символов')
     .max(40, 'Максимальная длина 40 символов'),
   repeatPassword: yup
     .string()
     .required('Это поле обязательно!')
     .oneOf([yup.ref('password')], 'Пароли не совпадают'),
-  agreement: yup
-    .boolean()
-    .oneOf([true], 'Необходимо согласиться, чтобы продолжить')
-    .required('Необходимо согласиться, чтобы продолжить'),
 });
 
 export const SignInSchema = yup.object().shape({
@@ -38,6 +26,6 @@ export const SignInSchema = yup.object().shape({
   password: yup
     .string()
     .required('Это поле обязательно!')
-    .min(6, 'Минимально 6 символов')
+    .min(5, 'Минимально 5 символов')
     .max(40, 'Максимальная длина 40 символов'),
 });
